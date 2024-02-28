c2d56bff6401c909a6196911b719c315406248a5
Yihuai Gao <yihuai@stanford.edu>
2024-02-27 19:29:56 -0800

    Update gst libraries for conda env

--- jetson_deploy/setup_python_env.sh
+++ jetson_deploy/setup_python_env.sh
@@ -1,2 +1,7 @@
 wget https://nvidia.box.com/shared/static/0h6tk4msrl9xz3evft9t0mpwwwkw7a32.whl -O torch-2.1.0-cp310-cp310-linux_aarch64.whl
 mamba env create -f jetson_environment.yaml
+sudo apt-get install libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev \
+    libgstreamer-plugins-good1.0-dev \
+    libgstreamer-plugins-bad1.0-dev
+cp /usr/lib/aarch64-linux-gnu/gstreamer-1.0/* /home/yihuai/miniforge3/envs/umi-jetson/lib/gstreamer-1.0
