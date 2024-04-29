3e254983c16e48d42e25bcbd3f6dce42ec7c40e9
huy-ha <hqh2101@columbia.edu>
2024-04-29 08:22:07 +0800

    Update jetson_environment

--- jetson_deploy/setup_python_env.sh
+++ jetson_deploy/setup_python_env.sh
@@ -1,4 +1,4 @@
-wget https://nvidia.box.com/shared/static/0h6tk4msrl9xz3evft9t0mpwwwkw7a32.whl -O torch-2.1.0-cp310-cp310-linux_aarch64.whl
+# wget https://nvidia.box.com/shared/static/0h6tk4msrl9xz3evft9t0mpwwwkw7a32.whl -O torch-2.1.0-cp310-cp310-linux_aarch64.whl
 mamba env create -f jetson_environment.yaml
 sudo apt-get install libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev \
